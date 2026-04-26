27e7b8e20681b32a4845119ef412ded9846c670a
IoanPop2010 <daveioanpop@gmail.com>
2026-04-26 15:38:31 +0300

    Update performanta Sun 04/26/2026 15:38:30.98

--- urca.bat
+++ urca.bat
@@ -1,9 +1,24 @@
 @echo off
-set /p msg="Mesaj Commit: "
+title GitHub Uploader - Services Ultra
+echo --- Incepere proces de sincronizare ---
+
+:: Initializare daca nu exista
+if not exist .git (
+    git init
+    git remote add origin https://github.com/IoanPop2010/serv.git
+)
+
+:: Adaugare fisiere
 git add .
+
+:: Mesaj commit automat cu data si ora
+set msg=Update performanta %date% %time%
 git commit -m "%msg%"
-git remote add origin https://github.com/IoanPop2010/serv.git >nul 2>&1
+
+:: Push catre GitHub
 git branch -M main
-git push -u origin main
-echo --- Upload Complet ---
+git push -u origin main --force
+
+echo.
+echo --- Sincronizare reusita pe https://github.com/IoanPop2010/serv ---
 pause
