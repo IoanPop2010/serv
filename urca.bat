d01c243a52e37ae548dfb6c8776f102604769947
IoanPop2010 <daveioanpop@gmail.com>
2026-04-26 15:43:35 +0300

    de toate

--- urca.bat
+++ urca.bat
@@ -4,21 +4,26 @@ echo --- Incepere proces de sincronizare ---
 
 :: Initializare daca nu exista
 if not exist .git (
+    echo [INFO] Initializare repository...
     git init
-    git remote add origin https://github.com/IoanPop2010/serv.git
+    git remote add origin https://github.com/IoanPop2010/planificarelidars.git
 )
 
 :: Adaugare fisiere
 git add .
 
-:: Mesaj commit automat cu data si ora
-set msg=Update performanta %date% %time%
-git commit -m "%msg%"
+:: Solicitare introducere mesaj commit de la tastatura
+echo.
+set /p descriere="Ce ai modificat? (scrie mesajul si apasa Enter): "
+
+:: Commit cu mesajul introdus
+git commit -m "%descriere%"
 
 :: Push catre GitHub
 git branch -M main
 git push -u origin main --force
 
 echo.
-echo --- Sincronizare reusita pe https://github.com/IoanPop2010/serv ---
+echo --- Sincronizare reusita pe https://github.com/IoanPop2010/planificarelidars ---
+echo --- Fisierul a fost incarcat cu mesajul: "%descriere%" ---
 pause
